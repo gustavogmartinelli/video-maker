cb84ba1420e9408616bc35c781010f8ad3c526be
Gustavo G. Martinelli <gustavogmartinelli@gmail.com>
2019-11-09 22:27:50 -0300

    feat: add watson integration

--- robots/text.js
+++ robots/text.js
@@ -1,11 +1,25 @@
 const algorithmia = require("algorithmia");
-const algorithmiaKey = require("../credentials/algorithmia.json").apikey;
 const sentenceBoundaryDetection = require("sbd");
+
+const algorithmiaKey = require('../credentials/algorithmia.json').apikey;
+const watsonApiKey = require('../credentials/watson_nlu.json').apikey;
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+
+
+const nlu = new NaturalLanguageUnderstandingV1({
+  iam_apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
 
 async function robot(content) {
   await fecthContenctFromWikipedia(content);
   sanitizeContent(content);
   breakContentIntoSentences(content);
+  limitMaximumSentences(content);
+  await fecthKeywordsOfAllSentences(content);
+  
 
   async function fecthContenctFromWikipedia(content) {
     const algorithmiaAuthenticaded = algorithmia(algorithmiaKey);
@@ -60,5 +74,38 @@ async function robot(content) {
         });      
     });
   }
+
+  function limitMaximumSentences(content) {
+    content.sentences = content.sentences.slice(0, content.maximumSetences);
+  }
+
+  async function fecthKeywordsOfAllSentences(content) {
+    for (const sentence of content.sentences){
+      sentence.keywords = await fecthWatsonAndReturnKeywords(sentence.text);
+    }
+  }
+
+  async function fecthWatsonAndReturnKeywords(sentence) {
+    return new Promise((resolve, reject) => {
+      nlu.analyze({
+        text: sentence,
+        features: {
+          keywords: {}
+        }
+      }, (error, response) => {
+       if(error) {
+         throw error;
+       }
+  
+       const keywords = response.keywords.map((keyword) => {
+            return keyword.text;     
+        });
+          
+        resolve(keywords)
+      });
+    });
+  }
+  
+
 }
 module.exports = robot;
